3638f63513c386a9200c6c391f5b590f9e0b500b
Бочков Максим <ocmax@mail.ru>
2017-01-24 23:42:39 +0300

    построена модель с построениями графиков

---
+++ limited growth model with catching/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace limited_growth_model_with_catching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(30, 84);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(743, 260);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Коэффициент\r\nвоспросизводства r";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Емкость ниши k";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Объем добычи E=const";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(160, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Оптимальный объем добычи равен";
+            this.label4.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(363, 38);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(507, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Исходный объем популяции";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(510, 38);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 396);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Построение графика и нахождение оптимальных параметров";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ limited growth model with catching/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace limited_growth_model_with_catching
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, Color> Colors = new Dictionary<int, Color>();
+        private int iteraror = 0;
+        private int length = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Colors.Add(0, Color.Black);
+            Colors.Add(1, Color.Blue);
+            Colors.Add(2, Color.Brown);
+            Colors.Add(3, Color.DarkGreen);
+            Colors.Add(4, Color.Gray);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r = ParseDouble(textBox1.Text);
+            double k = ParseDouble(textBox2.Text);
+            if (k < 0)
+            {
+                label4.Text = "Недопустимая емкость";
+                return;
+            }
+            double Y = ParseDouble(textBox3.Text);
+            double x0 = ParseDouble(textBox4.Text);
+            if (k < x0)
+            {
+                label4.Text = "Неввозможно впихнуть невпихуемое";
+                return;
+            }
+            Dictionary<int, Double> tabDictionary = new Dictionary<int, double>();
+            double x = 0;
+            tabDictionary.Add(0, x0);
+            for (int i = 1; i < length; i++)
+            {
+                x = tabDictionary[i - 1]*r*(1 - tabDictionary[i - 1]/k) - Y*tabDictionary[i - 1];
+
+                if (x <= 0)
+                    break;
+
+                tabDictionary.Add(i, x);
+            }
+            label4.Visible = true;
+            chart1.Series.Add("Рыбы" + iteraror);
+            chart1.Series[iteraror].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            chart1.Series[iteraror].Color = Colors[iteraror];
+
+
+            for (int i = 0; i < tabDictionary.Count; i++)
+                chart1.Series[iteraror].Points.AddXY(i, tabDictionary[i]);
+            iteraror++;
+            if (iteraror > 4)
+            {
+                DialogResult dialogResult = MessageBox.Show("Продолжить построение графиков?", "Переполнение графика", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    chart1.Series.Clear();
+                    iteraror = 0;
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    button1.Enabled = false;
+                }
+
+            }
+        }
+
+
+        public static double ParseDouble(string s)
+        {
+            double res;
+            s = s.Replace('.', ',');
+            Double.TryParse(s, out res);
+            return res;
+        }
+    }
+}
